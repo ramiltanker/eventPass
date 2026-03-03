e19be39c9bdfcf00e41b6c8ccc46b6c1ad9b06d6
Ramil Ashrafulin <r.ashrafulin@vk.team>
2026-03-03 23:34:42 +0300

    fix: prisma, feat: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { ValidationPipe } from '@nestjs/common'; // добавить
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,7 +12,17 @@ async function bootstrap() {
       whitelist: true,
       transform: true,
     }),
-  ); // добавить
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('EventPass API')
+    .setDescription('API for EventPass application')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') || 3000;
